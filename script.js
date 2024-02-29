a57e7ab10753e4461313b73b92508eebe79f9b60
rudycancax <44583578+RudyCancax@users.noreply.github.com>
2024-02-28 18:49:16 -0600

    Feat: Display first scene.

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 import * as THREE from "three";
 
+// CANVAS
+const canvas = document.querySelector('canvas.webgl');
+
 // SCENE
 const scene = new THREE.Scene();
 
-// GEOMETRY
+// OBJECT
 const geo = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color: '#0d0d1d'})
+const material = new THREE.MeshBasicMaterial({color: '#23f76b'})
 const mesh = new THREE.Mesh(geo, material);
 
-console.log('js working!!! ', THREE);
+scene.add(mesh);
+
+// CAMERA
+const camera = new THREE.PerspectiveCamera(35, 800/600);
+camera.position.z = 5;
+scene.add(camera);
+
+// RENDERER
+const renderer = new THREE.WebGLRenderer({canvas: canvas});
+
+renderer.setSize(800, 600);
+renderer.render(scene, camera);
