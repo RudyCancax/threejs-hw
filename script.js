688714a40f80105802e813b81a922d626249fe5c
rudycancax <44583578+RudyCancax@users.noreply.github.com>
2024-02-29 20:26:50 -0600

    Feat: First THREEJS Box

--- script.js
+++ script.js
@@ -8,14 +8,16 @@ const scene = new THREE.Scene();
 
 // OBJECT
 const geo = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color: '#23f76b'})
+const material = new THREE.MeshBasicMaterial({color: '#23f76b'});
 const mesh = new THREE.Mesh(geo, material);
 
 scene.add(mesh);
 
 // CAMERA
 const camera = new THREE.PerspectiveCamera(35, 800/600);
-camera.position.z = 5;
+camera.position.z = 7;
+camera.position.x = 1;
+camera.position.y = 1;
 scene.add(camera);
 
 // RENDERER
